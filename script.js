e2acd784dcb99905e72caafe9b7174403f9884f1
kotkot21 <abua6937@gmail.com>
2024-04-21 09:38:57 +0300

    final

---
+++ script.js
@@ -0,0 +1,7 @@
+function toggleMenu(){
+    const menu = document.querySelector(".menuLinks");
+    const icon = document.querySelector(".hamvurgerIcon");
+    menu.classList.toggle("open");
+    icon.classList.toggle("open");
+
+}
